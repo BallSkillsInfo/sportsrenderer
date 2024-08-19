3c6015082b890002cb2a0aaf3b6f65423124f689
Ohsey <admin@proton.me>
2024-08-19 23:59:40 +0100

    Rename project

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
-        .name = "BallSkillsInfoRenderer",
+        .name = "zigzag",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
